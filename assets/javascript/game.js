63b18de16ca1646c652260f92f1e0a17c7d26aa1
joel <newfoundfreedom@gmail.com>
2017-02-26 18:31:55 -0500

    final commit - everyting buttoned up and pretty

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -36,7 +36,7 @@ var ccGame = {
                 .addClass('col-xs-4 text-center')                                           //   assign it to take up 4 columns of its parent div
                 .appendTo('#game-board');                                                   //   place it within the game board area
             $('<img>')                                                                      // create an image element
-                .addClass('game-crystal')                                                   //   assign it the game-crystal class (which we will grab later for our click function)
+                .addClass('game-crystal img-zoom')                                          //   assign it the game-crystal class (which we will grab later for our click function)
                 .attr('src', crystalDisplayed)                                              //   place the randomly selected image in it
                 .data('crystalPoints', crystalValue)                                        //   assign the randomly selected value through the data attribute
                 .appendTo(crystalContainer);                                                //   place the image within the container div created above
@@ -52,27 +52,32 @@ var ccGame = {
             ccGame.playerScore += val;                          // add the clicked crystal's value to the players number
             $('#player-score').text(ccGame.playerScore);        // update the players number display
             if (ccGame.targetNum == ccGame.playerScore) {       // check to see if the game has been won by comparing the players score against the target number
-                ccGame.winCount++;                                // if so, iterate the win counter
+                ccGame.winCount++;                              // if so, iterate the win counter
                 $('#winModal').modal('show');                   // and show the win modal window
             }
             else if (ccGame.playerScore > ccGame.targetNum) {   // check to see if the game has been lost - if player score is greater than the target number
-                ccGame.lossCount++;                               // if so, then iterate the loss counter
+                ccGame.lossCount++;                             // if so, then iterate the loss counter
                 $('#lostModal').modal('show');                  // and show the loss modal window
             }
         })
 
-        $('#myModal').modal({
-            backdrop: 'static',
-            keyboard: false
-        })
-
         $('.play-again').on('click', function () {              // if the play-again button is pressed then
-            ccGame.startGame();                                   // run the start game function
+            ccGame.startGame();                                 // run the start game function
         })
 
         $('.exit').on('click', function () {                    // if an exit button is pressed then
             window.close();                                     // exit the current window
         })
+
+        $('.img-zoom').hover(function() {
+            $(this).addClass('transition');
+
+        }, function() {
+            $(this).removeClass('transition');
+        });
+
+
+
     }
 };
 
